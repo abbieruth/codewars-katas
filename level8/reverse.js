353bb633ca760a26481158ed75f84f196b20b9d5
Coding for Christ <72089733+abbieruth@users.noreply.github.com>
2022-04-20 11:34:34 -0400

    Added reverse string codewars challenge, 8kyu

---
+++ level8/reverse.js
@@ -0,0 +1,7 @@
+function solution(str){
+    // Convert to array using Array.from(string)
+    // Reverse array with array.reverse()
+    // Convert back to string using array.join('')?
+    // Return reversed string
+    return Array.from(str).reverse().join('')
+  }
