5ea9404a0a91194db5d71726dcb852e96155dbce
Coding for Christ <72089733+abbieruth@users.noreply.github.com>
2022-05-03 11:27:06 -0400

    Forgot to clean up code layout

--- level7/monotone.js
+++ level7/monotone.js
@@ -3,9 +3,9 @@
 
 var isMonotone = function(arr){
     for (let i = 0; i < arr.length; i++) {
-      if (arr[i - 1] > arr[i]) {
-        return false;
-      }
+        if (arr[i - 1] > arr[i]) {
+            return false;
+        }
     }
     return true;
-  }
+}
